7634bc5ccff9b354cb0398c2fd869f618603f116
CR7HARSHIT <hr2697020@gmail.com>
2025-01-18 02:03:57 +0530

    Vedio Container & Vedio card formed

--- src/components/VedioContainer.js
+++ src/components/VedioContainer.js
@@ -1,6 +1,37 @@
+import { useEffect, useState } from "react";
+import { APIKEY, VedioAPIURL } from "../utils/Constants";
+import Vedio from "./Vedio";
 const VedioContainer=()=>{
+	const [arr,setarr]=useState([]);
+	useEffect(()=>{
+   
+	vedioListFn()
+	},[])
+	const vedioListFn=async ()=>{
+		
+		
+		try {
+		  const response = await fetch(VedioAPIURL+APIKEY);
+	   console.log("Made API CALL")
+		  
+		  console.log(response); 
+		  const data = await response.json();
+		  console.log(`data::${data}`); 
+		  setarr(data.items)
+		} catch (error) {
+		  console.error('Error fetching data:', error);
+		}
+	  }
 	return (
-		<div>VedioContainer</div>
+		
+			<div className="flex flex-row flex-wrap">
+			{
+				arr.map((vedioinfo)=>{
+                   return (<Vedio Info={vedioinfo}/>)
+				})
+			}
+			</div>
+		
 	)
 }
 export default VedioContainer; 
