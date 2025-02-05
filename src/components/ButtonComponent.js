3185597027301699f2e187b827d7946f64b7d387
CR7HARSHIT <hr2697020@gmail.com>
2025-02-05 21:24:08 +0530

    Fixing btn height issue

--- src/components/ButtonComponent.js
+++ src/components/ButtonComponent.js
@@ -1,7 +1,7 @@
 const ButtonComponent=({value})=>{
     return (
        <div>
-		<button className=" m-2 p-2 font-bold border rounded-md bg-slate-100 hover:bg-slate-200">{value}</button>
+		<button className="whitespace-nowrap m-2 p-2 font-bold border rounded-md bg-slate-100 hover:bg-slate-200">{value}</button>
 	   </div>
 	)
 }
